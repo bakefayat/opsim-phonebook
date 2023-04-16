ba01fb4977bc6863b39b2652553523eeaa1a0b9e
bakefayat <bakefayat98@gmail.com>
2023-04-16 14:50:56 +0330

    nan changed to blank str.

--- excel_to_number.py
+++ excel_to_number.py
@@ -4,26 +4,24 @@ import pandas as pd
 
 def add_to_set():
     pan = pd.read_excel('phones.xlsx', header=1)
-    cell_value = pan.at[2, 'داخلی']
     df = pd.DataFrame(pan)
+    # fill merged units.
     df['واحد'].fillna(method='ffill', inplace=True)
-
     unit = df['واحد'].tolist()
-    position = df['سمت'].tolist()
-    name = df['نام'].tolist()
-    phone = df['داخلی'].tolist()
+    position = df['سمت'].fillna('').tolist()
+    name = df['نام'].fillna('').tolist()
+    phone = df['داخلی'].fillna('').to_list()
     units = []
     positions = []
     names = []
     phones = []
     for i in range(0, len(phone)):
-        if phone != 'nan':
+        if phone[i] != '':
             names.append(name[i])
             phones.append(phone[i])
             units.append(unit[i])
             positions.append(position[i])
     info = zip(units, positions, names, phones)
+
     print(list(info))
-
-
 add_to_set()
