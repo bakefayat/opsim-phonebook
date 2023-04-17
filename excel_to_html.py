7ff2836611f12b54940206e2110304c8d4246dcb
bakefayat <bakefayat98@gmail.com>
2023-04-17 07:44:05 +0330

    code a bit cleaned.

--- excel_to_html.py
+++ excel_to_html.py
@@ -1,11 +1,10 @@
-import numpy as np
 import pandas as pd
 
 
-def add_to_set():
+def excel_to_zip():
     pan = pd.read_excel('phones.xlsx', header=1)
     df = pd.DataFrame(pan)
-    # fill merged units.
+    # Fill merged units.
     df['واحد'].fillna(method='ffill', inplace=True)
     unit = df['واحد'].tolist()
     position = df['سمت'].fillna('').tolist()
@@ -15,6 +14,7 @@ def add_to_set():
     positions = []
     names = []
     phones = []
+    # Iterate all of valid phones
     for i in range(0, len(phone)):
         if phone[i] != '':
             names.append(name[i])
@@ -24,16 +24,22 @@ def add_to_set():
             units.append(unit[i].replace('ـ', ''))
             positions.append(position[i])
     info = zip(units, positions, names, phones)
-    
+    return info
+
+def zip_into_html(info):
     html_rows = ''
     # Iterate through the zip object
     for unit, position, name, phone in info:
         # Generate HTML table row
         html_row = f"<tr>\n\t<td>{unit}</td>\n\t<td>{position}</td>\n\t<td>{name}</td>\n\t<td>{phone}</td>\n</tr>\n"
         html_rows += html_row
+    return html_rows
 
+def write_to_html(html_rows):
     # Write to HTML file.
     with open('output.html', 'w', encoding='utf-8') as f:
         f.write(html_rows)
 
-add_to_set()
+zip_obj = excel_to_zip()
+inn_html = zip_into_html(zip_obj)
+write_to_html(inn_html)
